0fd87e4e6018deb6d4a66b99c95db56680ea3422
rmillett <rmillett@student.cccd.edu>
2017-10-24 15:30:39 -0700

    completed activity_music

--- app/src/main/java/edu/orangecoastcollege/cs273/alohamusic/MusicActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/alohamusic/MusicActivity.java
@@ -2,6 +2,7 @@ package edu.orangecoastcollege.cs273.alohamusic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MusicActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MusicActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+    }
+
+    public void playMedia(View view) {
+
     }
 }
